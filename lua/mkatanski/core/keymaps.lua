a057d464ac7c5ceab11dbd91f215d157c702f01e
Michał Katański <mkatanski@indust.co>
2024-08-03 12:45:07 +0200

    Map beginning and end of the line navigation

--- lua/mkatanski/core/keymaps.lua
+++ lua/mkatanski/core/keymaps.lua
@@ -16,3 +16,15 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- editor navigation
+
+-- Map Ctrl-E as CMD-right to move to the end of the line
+vim.api.nvim_set_keymap("n", "<C-e>", "$", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-e>", "<Esc>A", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-e>", "<Esc>g_", { noremap = true, silent = true })
+
+-- Map Ctrl-A as CMD-left to move to the beginning of the line
+vim.api.nvim_set_keymap("n", "<C-a>", "^", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-a>", "<Esc>I", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-a>", "<Esc>^", { noremap = true, silent = true })
--- lua/mkatanski/plugins/colorscheme.lua
+++ lua/mkatanski/plugins/colorscheme.lua
@@ -3,6 +3,13 @@ return {
 		"0xstepit/flow.nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
+			require("flow").setup({
+				transparent = true, -- Set transparent background.
+				fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
+				mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+				aggressive_spell = false, -- Display colors for spell check.
+			})
+
 			-- load the colorscheme here
 			vim.cmd([[colorscheme flow]])
 		end,
